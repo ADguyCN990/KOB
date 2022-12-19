229c2573f093fb885be12402b0e6c61304ef7160
ADguyCN990 <JHjinhui990@outlook.com>
2022-12-19 19:33:37 +0800

    try back-front port interact

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,30 +1,33 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
+  <h1>我的最爱：{{ name }}</h1>
   <router-view/>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import $ from 'jquery';
+import { ref } from 'vue';
 
-#nav {
-  padding: 30px;
-}
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+export default {
+  setup: () => {
+    let name = ref("");
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+    $.ajax({
+      url: "http://127.0.0.1:3000/pk/getbotinfo/",
+      type: "get",
+      success: resp => {
+        name.value = resp;
+      }
+    })
+
+    return {
+      name
+    }
+  }
 }
+</script>
+
+<style scoped>
 </style>
+
+
