7bc3ac88701c7deb25e8d1c798c8ddaedf591391
ADguyCN990 <JHjinhui990@outlook.com>
2022-12-21 17:07:37 +0800

    finish mysql config

--- backend/src/main/java/com/example/backend/controller/pk/IndexController.java
+++ backend/src/main/java/com/example/backend/controller/pk/IndexController.java
@@ -3,10 +3,9 @@ package com.example.backend.controller.pk;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
-@RequestMapping ("/pk/")
 public class IndexController {
 
-    @RequestMapping("index/")
+    @RequestMapping("/")
     public String index() {
         return "pk/index.html";
     }
